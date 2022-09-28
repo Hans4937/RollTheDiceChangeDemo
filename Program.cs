6ba1737c082f53a0afe87f9bca245981e87a1cbb
Hans4937 <haje49370@edu.ucl.dk>
2022-09-28 09:07:16 +0200

    "Refactoring code"

--- Program.cs
+++ Program.cs
@@ -36,17 +36,14 @@ namespace RollTheDiceLearn
             Console.WriteLine("You are running the game in " + mode + " mode");
 
             Random rand = new Random();
-            bool cont = true;
+            bool proceed = true;
 
             int dice_1_1 = 0;
             int dice_1_2 = 0;
             int dice_2_1 = 0;
             int dice_2_2 = 0;
-            bool player_equal;
-            bool opponent_equal;
-            int countEquals = 0;
 
-            while (cont)
+            while (proceed)
             {
                 Console.WriteLine("You roll the dices - press <Return> to continue:");
                 Console.ReadLine();
@@ -65,54 +62,66 @@ namespace RollTheDiceLearn
                     dice_2_1 = int.Parse(args[2]);
                     dice_2_2 = int.Parse(args[3]);
                 }
-                player_equal = dice_1_1 == dice_1_2;
-                opponent_equal = dice_2_1 == dice_2_2;
-
                 
                 Console.WriteLine(string.Format("Your roll: {0} - {1}", dice_1_1.ToString(), dice_1_2.ToString()));
                 Console.WriteLine(string.Format("My roll: {0} - {1}", dice_2_1.ToString(), dice_2_2.ToString()));
 
+                //Make a call to method
+                proceed = GameMaster(dice_1_1, dice_1_2, dice_2_1, dice_2_2);
+                Console.ReadLine();
+            }
+        }
+        
+        static bool GameMaster(int dice_1_1, int dice_1_2, int dice_2_1, int dice_2_2)
+        {
+            bool player_equal;
+            bool opponent_equal;
+            int countEquals = 0;
+            bool proceed = true;
+
+            player_equal = dice_1_1 == dice_1_2;
+            opponent_equal = dice_2_1 == dice_2_2;
+
+            if (player_equal)
+            {
+                countEquals++;
+            }
+            if (opponent_equal)
+            {
+                countEquals++;
+            }
+
+            if (countEquals == 1)
+            {
                 if (player_equal)
                 {
-                    countEquals++;
+                    Console.WriteLine("You win!");
                 }
                 if (opponent_equal)
                 {
-                    countEquals++;
+                    Console.WriteLine("I win!");
                 }
-
-                if (countEquals == 1)
-                {
-                    if (player_equal)
-                    {
-                        Console.WriteLine("You win!");
-                    }
-                    if (opponent_equal)
-                    {
-                        Console.WriteLine("I win!");
-                    }
-                    cont = false;
-                }
-
-                if (countEquals == 2)
-                {
-                    if ((dice_1_1 + dice_1_2) > (dice_2_1 + dice_2_2))
-                    {
-                        Console.WriteLine("You win!");
-                        cont = false;
-                    }
-                    if ((dice_1_1 + dice_1_2) < (dice_2_1 + dice_2_2))
-                    {
-                        Console.WriteLine("I win!");
-                        cont = false;
-                    }
-                    if ((dice_1_1 + dice_1_2) == (dice_2_1 + dice_2_2))
-                    {
-                        Console.WriteLine("Draw! Continuing");
-                    }
-                }
-                Console.ReadLine();
+                proceed = false;
             }
+
+            if (countEquals == 2)
+            {
+                if ((dice_1_1 + dice_1_2) > (dice_2_1 + dice_2_2))
+                {
+                    Console.WriteLine("You win!");
+                    proceed = false;
+                }
+                if ((dice_1_1 + dice_1_2) < (dice_2_1 + dice_2_2))
+                {
+                    Console.WriteLine("I win!");
+                    proceed = false;
+                }
+                if ((dice_1_1 + dice_1_2) == (dice_2_1 + dice_2_2))
+                {
+                    Console.WriteLine("Draw! Continuing");
+                }
+            }
+            return true;
         }
     }
 }
