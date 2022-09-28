a454397da9b8fa19a8b14739d9ab50bdde531d30
Hans4937 <haje49370@edu.ucl.dk>
2022-09-28 09:54:32 +0200

    Refactoring and making a new class

--- Program.cs
+++ Program.cs
@@ -67,61 +67,9 @@ namespace RollTheDiceLearn
                 Console.WriteLine(string.Format("My roll: {0} - {1}", dice_2_1.ToString(), dice_2_2.ToString()));
 
                 //Make a call to method
-                proceed = GameMaster(dice_1_1, dice_1_2, dice_2_1, dice_2_2);
+                proceed = Gamerules.GameMaster(dice_1_1, dice_1_2, dice_2_1, dice_2_2);
                 Console.ReadLine();
             }
-        }
-        
-        static bool GameMaster(int dice_1_1, int dice_1_2, int dice_2_1, int dice_2_2)
-        {
-            bool player_equal;
-            bool opponent_equal;
-            int countEquals = 0;
-            bool proceed = true;
-
-            player_equal = dice_1_1 == dice_1_2;
-            opponent_equal = dice_2_1 == dice_2_2;
-
-            if (player_equal)
-            {
-                countEquals++;
-            }
-            if (opponent_equal)
-            {
-                countEquals++;
-            }
-
-            if (countEquals == 1)
-            {
-                if (player_equal)
-                {
-                    Console.WriteLine("You win!");
-                }
-                if (opponent_equal)
-                {
-                    Console.WriteLine("I win!");
-                }
-                proceed = false;
-            }
-
-            if (countEquals == 2)
-            {
-                if ((dice_1_1 + dice_1_2) > (dice_2_1 + dice_2_2))
-                {
-                    Console.WriteLine("You win!");
-                    proceed = false;
-                }
-                if ((dice_1_1 + dice_1_2) < (dice_2_1 + dice_2_2))
-                {
-                    Console.WriteLine("I win!");
-                    proceed = false;
-                }
-                if ((dice_1_1 + dice_1_2) == (dice_2_1 + dice_2_2))
-                {
-                    Console.WriteLine("Draw! Continuing");
-                }
-            }
-            return true;
         }
     }
 }
